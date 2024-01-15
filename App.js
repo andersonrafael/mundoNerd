1bf595c231709d75b1e8b505cdac9e9d8531114d
Francisco Anderson Rafael da Silva <andersonrafael@users.noreply.github.com>
2024-01-15 11:03:32 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { StatusBar, SafeAreaViewView, SafeAreaView } from 'react-native';
+
+import Cesta from './src/telas/Cesta';
+
+
+export default function App() {
+  return (
+    <SafeAreaView >
+      <StatusBar />
+      <Cesta />
+
+    </SafeAreaView>
+  );
+}
+
+
